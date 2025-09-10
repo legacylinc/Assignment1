e2e7e505a3976b13bbe53a51dda33a7caa545df3
legacylinc <legacylinc0@gmail.com>
2025-09-10 15:30:09 +0200

    push

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+# This is a simple Python program to demonstrate
+# the use of variables, constants, and the print function.
+
+# Variables (they can change)
+name = "Lincoln"     # a variable holding a text (string)
+age = 25             # a variable holding a number (integer)
+
+# Constant (by convention, written in ALL CAPS to show it should not change)
+PI = 3.14159
+
+# Using the print function to display information
+print("Hello, my name is", name)
+print("I am", age, "years old.")
+print("The value of PI is:", PI)
+
+# Doing a calculation with variables and constants
+radius = 5
+area = PI * (radius ** 2)   # formula: area of a circle = π * r²
+print("The area of a circle with radius", radius, "is:", area)
